1cf38efe0481e7a50c4a45a339f631baa217336d
IqbaalTaufik <58700500+IqbaalTaufik@users.noreply.github.com>
2022-12-04 11:33:22 +0000

    1

--- main.py
+++ main.py
@@ -24,31 +24,34 @@ scissor = '''    _______
 pilihan = [rock, paper, scissor]
 print("0 for rock, 1 for paper, and 2 for scissor")
 choice = int(input("What your choose? "))
-choice1 = pilihan[choice]
-
-choice2 = random.randint(0, 2)
-choicee2 = pilihan[choice2]
-
-print("your choice : ")
-print(f"{choice1}\n")
-print("==========================")
-print("Computer choice : ")
-print(f"{choicee2}\n")
-print("==========================")
-
-if choice == choice2:
-  print("You both draw")
-elif choice == 0 and choice2 == 1:
-  print("You Lose")
-elif choice == 1 and choice2 == 2:
-  print("You Lose")
-elif choice == 2 and choice2 == 0:
-  print("You Lose")
-elif choice == 2 and choice2 == 1:
-  print("You Win")
-elif choice == 0 and choice2 == 2:
-  print("You Win")
-elif choice == 1 and choice2 == 0:
-  print("You Win")
+if choice >= 3 or choice <0:
+  print("Invalid Number,Game End")
 else:
-  print("Type the invalid number, you got nothing you lose! ")
+  choice1 = pilihan[choice]
+  
+  choice2 = random.randint(0, 2)
+  choicee2 = pilihan[choice2]
+  
+  print("your choice : ")
+  print(f"{choice1}\n")
+  print("==========================")
+  print("Computer choice : ")
+  print(f"{choicee2}\n")
+  print("==========================")
+  
+  if choice == choice2:
+    print("You both draw")
+  elif choice == 0 and choice2 == 1:
+    print("You Lose")
+  elif choice == 1 and choice2 == 2:
+    print("You Lose")
+  elif choice == 2 and choice2 == 0:
+    print("You Lose")
+  elif choice == 2 and choice2 == 1:
+    print("You Win")
+  elif choice == 0 and choice2 == 2:
+    print("You Win")
+  elif choice == 1 and choice2 == 0:
+    print("You Win")
+  else:
+    print("Type the invalid number, you got nothing you lose! ")
